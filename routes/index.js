e9a4a161fbe8d9cfec1426a2c45b643f10fc46c4
Manuel Beltran <manueldbeltranc@gmail.com>
2023-10-31 13:30:12 -0700

    finish project with basic styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express')
 var router = express.Router()
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello world',
+    user: 'Charles',
+    added: new Date(),
+  },
+]
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Mini Messageboard', messages: messages })
+})
+
+router.get('/new', (req, res, next) => {
+  res.render('form')
+})
+
+router.post('/new', (req, res, next) => {
+  if (!req.body.name || !req.body.message) {
+    res.status(400).send('Messages must have a name and message.')
+    return
+  }
+
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  })
+
+  res.redirect('/')
 })
 
 module.exports = router
